f969e16b295f51f89878f3a9e98840b990fa92a1
Prashant Patil <prashant.patil@niyuj.com>
2016-08-07 13:17:14 +0530

    Update params.php

--- common/config/params.php
+++ common/config/params.php
@@ -1,15 +1,1 @@
-<?php
-return [
-    'adminEmail' => 'support@partyanimals.in',
-    'supportEmail' => 'support@partyanimals',
-    'user.passwordResetTokenExpire' => 3600,
-      'blogTitle' => 'Partyanimals.in',
-    'blogTitleSeo' => 'Simple Blog based on Yii2',
-    'blogFooter' => 'Copyright &copy; ' . date('Y') . ' by ahuasheng on Yii2. All Rights Reserved.',
-    'blogPostPageCount' => '10',
-    'blogLinks' => [
-        'Google' => 'http://www.google.com',
-        'Funson86 Blog' => 'http://github.com/funson86/yii2-blog',
-    ],
-    'blogUploadPath' => 'upload/', //default to frontend/web/upload
-];
+
